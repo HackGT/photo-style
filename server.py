3726c566e5855833fb916a0c889032f04b91a9ca
Marc Marone <ruyi.marone@gmail.com>
2018-10-20 20:37:06 -0400

    fixups

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ import os
 import json
 import base64
 
-import skimage 
+import skimage
 
 # from fast_neural_style.forward import forward_pass
 import DetectronPytorch.tools._init_paths
@@ -93,7 +93,7 @@ def convert_encoded():
     mask, scored_masks, styled_images = segment_and_style(models, detectron, im)
     numpy_image = np.array(im)
     print("Found {} people".format(len(scored_masks)))
-    
+
     image_cache['source_im'] = numpy_image.copy()
     image_cache['scored_masks'] = [m.copy() for m in scored_masks]
     image_cache['styled_images'] = [i.copy() for i in styled_images]
@@ -108,9 +108,9 @@ def convert_encoded():
         buffered = BytesIO()
         i.save(buffered, format="JPEG")
         filter_payload.append(base64.b64encode(buffered.getvalue()).decode('ascii'))
-    
-    resized_mask = F.max_pool2d(torch.FloatTensor(mask).unsqueeze(dim=0), (3, 3), stride=3) 
-    resized_mask = resized_mask.squeeze().numpy().astype(int) 
+
+    resized_mask = F.max_pool2d(torch.FloatTensor(mask).unsqueeze(dim=0), (3, 3), stride=3)
+    resized_mask = resized_mask.squeeze().numpy().astype(int)
 
     im = im.resize((int(im.size[0] / 3), int(im.size[1] / 3)))
     buffered = BytesIO()
@@ -127,25 +127,18 @@ def prepare_final():
     print(request_data)
     email = request_data['email']
     mix_info = request_data['mixInfo']
-    # Comment this back in when hooking up to frontend
-    if not email or not mix_info:
-        return jsonify({'error': "Corrupted mix info"})
-
-    print(len(image_cache['scored_masks'])) 
-
 
     # EDIT HERE
     # final_filename = "cheetos01.png" # high_res_mix(mix_info) # fill in this
     out = image_cache['source_im']
     for mask_ix, filter_ix in mix_info.items():
         mask_ix = int(mask_ix)
-        mask = image_cache['scored_masks'][mask_ix] 
+        mask = image_cache['scored_masks'][mask_ix]
         mask = (mask != 0)
         out, _, _ = apply_binary_mask(out, image_cache['styled_images'][filter_ix - 1], mask)
-    
+
     out_image = Image.fromarray(out)
     out_image.save('hello.jpg')
-
 
     # final_res should be in file_name - rename to some unique identifier PER photo
     # bucket initialized from console
@@ -154,7 +147,10 @@ def prepare_final():
     client = storage.Client()
     bucket = client.get_bucket(bucket_name)
     blob = bucket.blob(object_name)
-      
+
+    #crop the bottom edge to improve quality, slightly
+    out_image = out_image.crop((0, 0, 2000, 1293))
+
     stream = BytesIO()
     out_image.save(stream, format="JPEG")
     stream.seek(0)
@@ -162,6 +158,7 @@ def prepare_final():
     blob.upload_from_file(stream, content_type="image/jpg", client=client)
     blob.make_public()
     gcloud_link = "http://storage.googleapis.com/{}/{}".format(bucket_name, object_name)
+    print(gcloud_link)
     sg = sendgrid.SendGridAPIClient(apikey=os.environ['SENDGRID_API_KEY'])
     from_email = Email(os.environ["FROM_EMAIL"])
     to_email = Email(email)
@@ -169,9 +166,6 @@ def prepare_final():
     content = Content("text/html", '<a href="{}">Here</a> is a link to your photo. Have a nice day!'.format(gcloud_link))
     mail = Mail(from_email, subject, to_email, content)
     response = sg.client.mail.send.post(request_body=mail.get())
-    # print(response.status_code)
-    # print(response.body)
-    # print(response.headers)
 
     return jsonify(success=True)
 
@@ -187,5 +181,4 @@ if __name__ == '__main__':
         models.append(model)
         model_map[model_name] = model
 
-    print(model_map)
     app.run(debug=False, port=8080, host='0.0.0.0')
